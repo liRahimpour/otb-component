a471b36af5356633fc72446fe9e47f66ec7f48b5
Ali Rahimpour <li.rahimpour@gmail.com>
2022-01-13 23:12:58 +0100

    init csvtojson

---
+++ csvtojson/src/main/java/com/asuntarin/csvtojson/CsvtojsonApplication.java
@@ -0,0 +1,13 @@
+package com.asuntarin.csvtojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvtojsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvtojsonApplication.class, args);
+    }
+
+}
---
+++ csvtojson/src/test/java/com/asuntarin/csvtojson/CsvtojsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asuntarin.csvtojson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvtojsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
